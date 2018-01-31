56ee6b0445620d8e4274a49a8605c064f23ffb44
David Allen <davideallen7@gmail.com>
2018-01-31 11:11:41 -1000

    generated link scaffold

---
+++ test/system/links_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class LinksTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit links_url
+  #
+  #   assert_selector "h1", text: "Link"
+  # end
+end
