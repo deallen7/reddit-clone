422ef5727bc885213994ceb56281f08f122187c5
David Allen <davideallen7@gmail.com>
2018-01-31 20:05:07 -1000

    added name field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201043226) do
+ActiveRecord::Schema.define(version: 20180201054100) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "link_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180201043226) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
